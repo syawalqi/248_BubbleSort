c83ba818415d8416fbfbe473aaa1238576ea5b7b
syawalqi <galihmaulanasyawalqi@gmail.com>
2024-03-07 15:55:57 +0700

    menabah kan void display

--- Logika1/Logika1.cpp
+++ Logika1/Logika1.cpp
@@ -42,6 +42,19 @@ void bubblesortarray() { //procedur untuk mengurutkan array
 
     } while (pass < n - 1); //step5
     
+}
+
+void display() {
+    cout << endl;
+    cout << "================" << endl;
+    cout << "Element array yang telah tersusun" << endl;
+    cout << "================" << endl;
+    for (int j = 0; j < n; j++) {
+        cout << arr[j] << endl; // output setiap element array pada garis baru
+
+    }
+    cout << "jumlah pass = " << n - 1 << endl; // menampilkan total
+    cout << endl;
 
 }
 
