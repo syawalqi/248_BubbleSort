da73868d216173751cbf98f57c23ecefe6d4b829
syawalqi <galihmaulanasyawalqi@gmail.com>
2024-03-07 15:49:30 +0700

    tambah temporary

--- Logika1/Logika1.cpp
+++ Logika1/Logika1.cpp
@@ -27,8 +27,24 @@ void input() {
 }
 
 void bubblesortarray() { //procedur untuk mengurutkan array
-    int pass = 1;
+
+    int pass = 1; // step 1
+    do {
+        for (int j = 0; j <= n - 1 - pass; j++) { //step 2
+            if (arr [j] > arr[j +1]) { // step3
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+
+            } 
+        }
+        pass = pass + 1; // step 4
+
+    } while (pass < n - 1); //step5
+    
+
 }
+
 
 int main()
 {
